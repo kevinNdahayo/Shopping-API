4f6042bbd4951138104f780064edc3aec492d054
kevinNdahayo <ndahayokevin1@gmail.com>
2022-03-24 10:13:03 -0700

    Removed CorsHandling controller

--- app.js
+++ app.js
@@ -5,30 +5,17 @@ const app = express()
 const productRoutes = require('./routes/products');
 const orderRoutes = require("./routes/orders")
 
+// Importing CORS Middleware
+const corsMiddleware = require('./utils/corsOptions')
+
 // importing Error handling controllers
 const { errorMessage, errorMessageResponse } = require("./Controllers/errorHandling.controllers")
 
 // Application Middlewares
+app.use(corsMiddleware)
 app.use(express.json())
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
-// Handling CORS errors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*")
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    
-    if(req.method === 'OPTIONS'){
-        res.header(
-            'Access-Control-Allow-Methods',
-            'PUT, POST, PATCH, DELETE'
-        )
-        return res.status(200).json({});
-
-    }
-})
 app.use(errorMessage)
 app.use(errorMessageResponse)
 module.exports = app;
